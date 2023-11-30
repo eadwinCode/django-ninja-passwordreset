ad2aab6642c80ce99af54d833454429752773649
Tochukwu <ezeudoh.tochukwu@gmail.com>
2023-11-30 11:21:07 +0100

    1.2.5

--- django_rest_passwordreset/__init__.py
+++ django_rest_passwordreset/__init__.py
@@ -1,4 +1,4 @@
 """ An extension of django ninja framework, providing a configurable password reset strategy"""
-__version__ = "1.2.4"
+__version__ = "1.2.5"
 
 default_app_config = "django_rest_passwordreset.apps.DjangoRestPasswordResetConfig"
